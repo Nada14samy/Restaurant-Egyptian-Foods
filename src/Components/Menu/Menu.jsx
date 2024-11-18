7e01bc05753d2f6712f58fded66c77a35a829336
Nada14samy <nm5135787@gmail.com>
2024-11-18 04:10:02 +0200

    Update Menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React ,{useState , useEffect} from 'react';
+import {Link} from "react-router-dom";
+// api
+import axios from "axios";
+import API_BASE_URL from '../../API.jsx';
 // component
-import FetchDataCard from "../FetchDataCard/FetchDataCard.jsx";
-
+import Card from "../Card/Card.jsx";
 
 function Menu() {
-
-    const handleDataFetch = (fetchedData) => {
-        console.log("set data" , fetchedData);
+    const [card, setCard] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [category , setCategory] = useState([]);
+    const [Button , setButton] = useState("");
+    const [Error , setError] = useState("");
+   
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`${API_BASE_URL}/items/menu/${Button}`);
+        setCard(res.data.result);
+        let categoryFilter = [...new Set(res.data.result.map(item => item.Category))];
+        setCategory(categoryFilter);
+      } catch (err) {
+        setError("please try again later");
+      } finally {
+        setIsLoading(false);
+      }
     };
+    fetchData();
+  }, []);
 
-    const Button = ["Drinks" , "japenas" , "Lunch" , "Dinner" , "italin"];
+    const HandleClickButton = (event)=>{
+        setButton(event.target.textContent);
+    }
+    
   
   return (
     <>
@@ -18,15 +41,40 @@ function Menu() {
             <div className="w-10/12 flex flex-col justify-center items-center  max-lg:w-full max-md:w-11/12">
             <div className="w-full flex justify-center items-center gap-10 flex-wrap mb-10">
                 {
-                    Button.map((item , index)=>(
-                        <button key={index} className={`px-10 text-[20px] font-medium py-2 rounded-[25px] border-yellow border-[2px] border-solid `}>
+                    category.map((item , index)=>(
+                        <button onClick={HandleClickButton} key={index} className={`btn-${index} px-10 text-[20px] font-medium py-2 rounded-[25px] border-yellow border-[2px] border-solid `}>
                             {item}
                         </button>
                     ))
                 }
+                <Link to="/menu" className={`px-10 text-[20px] font-medium py-2 rounded-[25px] bg-yellow text-lightWhite border-yellow border-[2px] border-solid `}>
+                    All
+                </Link>
             </div>
             <div className="w-full flex items-center lg:justify-center gap-10 flex-wrap my-5 max-md:gap-5 max-lg:justify-center  ">
-                 <FetchDataCard onDataFetch={handleDataFetch} />
+            {
+                Error ? (
+                    <>
+                    Error
+                    </>
+                ) : isLoading ? (
+                    <>
+                    Loading...
+                    </>
+                ) : (
+                    card.map((item, index) => (
+                    <Card 
+                        key={index} 
+                        Id={item._id}
+                        Salary={item.Salary} 
+                        Image={item.Image} 
+                        Name={item.Name} 
+                        Description={item.Description} 
+                        TotalRating={item.TotalRating} 
+                    />
+                    ))
+                )
+                }
             </div>
             </div>
         </section>
--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -1,83 +1,3 @@
-// import React, {useState , useEffect} from 'react';
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react';
-
-// //  api
-// import axios from "axios";
-// import API_BASE_URL from '../../API.jsx';
-// // Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-
-// // import required modules
-// import { Pagination } from 'swiper/modules';
-
-// // component 
-// import Card from "../Card/Card.jsx";
-
-// function Order() {
-//   const [card , setCard] = useState([]);
-//   const [IsLoading , setIsLoading] = useState(true);
- 
-//   useEffect(()=>{
-//       const fetchData = async ()=>{
-//           try{
-//               const res = await axios.get(`${API_BASE_URL}/items/menu`);
-//               setCard(res.data.result);
-//               console.log(res.data.result);
-//           }catch(err){
-//               console.log(err);
-//           }finally{
-//               setIsLoading(false);
-//           }
-//       };
-//       fetchData();
-//   }, []);
-  
-//   return (
-//     <>
-//       <section className="w-full  h-screen flex flex-col justify-center items-center my-10">
-//       <div className="w-[70%] flex flex-col justify-center items-center">
-//         <h3 className="text-[50px] font-medium text-center max-md:text-2xl mb-8">
-//           Our Popular <span className="text-lightRed">House</span> Foods
-//         </h3>
-//         <p className="font-medium text-gray text-[40px] max-md:text-2xl">Order Now</p>
-//       </div>
-
-//         <div className="flex w-[95%] h-full justify-around pl-9 max-md:pl-5 items-center gap-8 overflow-hidden">
-        
-//           <Swiper
-//             slidesPerView={4}
-//             spaceBetween={30}
-//             pagination={{
-//               clickable: true,
-//             }}
-//             modules={[Pagination]}
-//             className="mySwiper p-4"
-//           >
-//             {
-//               IsLoading ?
-//               <>Loading ...</>
-//               :
-//            card.map((item , index)=>{
-//               <SwiperSlide >
-//                 <Card key={index} Salary={item.Salary} Image={item.Image} Name={item.Name} Description={item.Description} TotalRating={item.TotalRating}  />
-//               </SwiperSlide>
-//             }) 
-             
-//             }
-            
-                 
-                    
-//             </Swiper>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Order;
-
 import React, { useState, useEffect } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -91,12 +11,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/free-mode';
 
-
-// import required modules
-import { Pagination } from 'swiper/modules';
-
 // component 
-import Card from "../Card/Card.jsx";
 import { FreeMode, Pagination } from 'swiper/modules';
 // import { RxArrowBottomRight } from 'react-icons/rx';
 import Card from "../Card/Card.jsx";
